8303701f9b26d509ad75537a80d4ca4e15bb0de0
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-12-26 10:20:50 +0100

    update

--- crud.js
+++ crud.js
@@ -3,7 +3,7 @@ const db = require('./db.service');
 // CREATE
 const createItem = (name, description, callback) => {
     const sql = `INSERT INTO items (name, description) VALUES (?, ?)`;
-    db.run(sql, [name, description], function(err) { 
+    db.run(sql, [name, description], function(err) {
         callback(err, { id: this.lastID });
     });
 }
