d1f47b311c5c00bbd15ba6c2bd1ed00f258c9a26
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-12-27 18:28:00 +0100

    index

--- db.service.js
+++ db.service.js
@@ -1,17 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
-const dbName = 'myDatabase.db';
+const dbName = 'data.db';
 
 const db = new sqlite3.Database(dbName, (err) => {
     if (err) {
         console.error(err.message);
     } else {
-        console.log('Database Connected');
         db.run('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT)', (err) => {
             if (err) {
                 console.error(err.message);
-            } else {
-                console.log('Table Created or already exists!');
-            }
+            } 
         });
     }
 });
--- index.js
+++ index.js
@@ -55,6 +55,7 @@ app.delete('/:tableName/:id', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log(`Server started on port 3000!`);
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}!`);
 });
